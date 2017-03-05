bc0543ac8783511252bc402e19fc0713987b0d4e
Arnav25 <arnav.chakraborty@iiitb.org>
2017-03-05 12:15:00 +0530

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,7 @@ module.exports=function(app)
 	var homeRoute=App.route('homeroute')
 	app.get("/",homeRoute.home)
 	var getstate=App.route('getstate');
-	app.get("/state/:var",getstate.state);
+	//app.get("/state/:var",getstate.state);
 	var transaction=App.route('transact')
 	app.get('/transaction',transact.trans);
 
